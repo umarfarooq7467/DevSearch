df6c007bc7e3055faa1c1265be52d3ba0f0496e5
umarfarooq7467 <umerfarooq612@gmail.com>
2023-01-11 21:45:39 +0500

    first commit

---
+++ users/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+from django.contrib.auth.models import User
+import uuid
+
+
+# Create your models here.  
+
+class Profile(models.Model): 
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+    name = models.CharField(max_length=150, blank=True, null=True)
+    email = models.EmailField(max_length=500, blank=True, null=True)
+    username = models.CharField(max_length=150, blank=True, null=True)
+    location = models.CharField(max_length=200, blank=True, null=True)
+    short_intro = models.CharField(max_length=200, blank=True, null=True)
+    bio = models.TextField(null=True, blank=True)  
+    profile_image = models.ImageField(null=True, blank=True, upload_to='profiles/', default='profiles/user-default.png')
+    
+    social_github = models.CharField(max_length=200, blank=True, null=True)
+    social_linkedin = models.CharField(max_length=200, blank=True, null=True)
+    social_twitter = models.CharField(max_length=200, blank=True, null=True)
+    social_youtube = models.CharField(max_length=200, blank=True, null=True)
+    social_website = models.CharField(max_length=200, blank=True, null=True)
+    created = models.DateField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+
+    def __str__(self):
+        return str(self.username)
+
+class Skill(models.Model):
+    owner = models.ForeignKey(Profile, on_delete=models.CASCADE, blank=True, null=True)
+    name = models.CharField(max_length=150, blank=True, null=True)
+    description = models.TextField(null=True, blank=True)
+    created = models.DateField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return str(self.name)
+
+
---
+++ users/views.py
@@ -0,0 +1,92 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from .models import Profile
+from django.contrib.auth.decorators import login_required
+from .forms import CustomUserCreationForm, ProfileForm 
+# Create your views here.
+
+def loginUser(request):
+    page = 'login'
+    if request.user.is_authenticated:
+        return redirect('profiles')
+
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, "Username doesn't exist")
+
+        user = authenticate(request, username=username, password=password)
+
+
+        if user is not None:
+            login(request, user)
+            return redirect('profiles')
+        else:
+            messages.error(request, 'Username or Password is incorrect')
+  
+    return render(request, 'users/login_register.html')
+
+def logoutUser(request):
+    logout(request)
+    messages.info(request, "User was logged out")
+    return redirect('login')
+
+
+def registerUser(request):
+    page = 'register'
+    form = CustomUserCreationForm()
+
+
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+
+            messages.success(request, 'User Account was created')
+
+            login(request, user)
+            return redirect('profiles')
+
+        else:
+            messages.success(request, 'An Error has occured during registraiton')
+
+    context = {'page': page, 'form': form}
+    return render(request, 'users/login_register.html', context)
+
+def profiles(request):
+    profiles = Profile.objects.all()
+    context = {'profiles': profiles}
+    return render(request, 'users/profiles.html', context)
+
+def userProfile(request, pk):
+    profile = Profile.objects.get(id=pk)
+    topSkills = profile.skill_set.exclude(description__exact="")
+    otherSkills = profile.skill_set.filter(description="")
+
+    context = {'profile': profile, 'topSkills': topSkills, 'otherSkills': otherSkills}  
+    return render(request, 'users/user-profile.html', context)
+
+@login_required(login_url='login')
+def userAccount(request):  
+    profile = request.user.profile
+    skills = profile.skill_set.all()
+    projects = profile.project_set.all()
+
+    context = {'profile': profile, 'skills': skills, 'projects':projects}
+    return render(request, 'users/account.html', context)
+
+@login_required(login_url='login') 
+def editAccount(request):
+    form = ProfileForm()
+    context = {'form': form}
+    return render(request, 'users/profile_form.html', context)
